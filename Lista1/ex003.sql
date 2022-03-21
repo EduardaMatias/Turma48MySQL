4023a7e2f3f9ddbfddd4fab9bcebdc3eddcf15e8
Eduarda <85466761+EduardaMatias@users.noreply.github.com>
2022-03-21 17:16:43 -0300

    Update ex003.sql

--- Lista1/ex003.sql
+++ Lista1/ex003.sql
@@ -13,7 +13,7 @@ Ao término atualize um dado desta tabela através de uma query de atualização
 
 CREATE DATABASE db_eastHigh;
 
-use db_eastHigh;
+USE db_eastHigh;
 
 CREATE TABLE tb_alunos(
 	id bigint auto_increment,
@@ -26,33 +26,33 @@ CREATE TABLE tb_alunos(
 	primary key(id)
 );
 
-insert into tb_alunos(nome, matricula, cpf, email, nota) 
-values ("Troy Bolton", "25154565351", "52515874698", "troy.bolton@aluno.com", 7.5);
+INSERT INTO tb_alunos(nome, matricula, cpf, email, nota) 
+VALUES ("Troy Bolton", "25154565351", "52515874698", "troy.bolton@aluno.com", 7.5);
 
-insert into tb_alunos(nome, matricula, cpf, email, nota) 
-values ("Gabriella Montez", "5848715423", "52545635814", "gabriella.montez@aluno.com", 9.5);
+INSERT INTO tb_alunos(nome, matricula, cpf, email, nota) 
+VALUES ("Gabriella Montez", "5848715423", "52545635814", "gabriella.montez@aluno.com", 9.5);
 
-insert into tb_alunos(nome, matricula, cpf, email, nota) 
-values ("Sharpey Evans", "25847986351", "25436598745", "sharpey.evans@aluno.com", 10);
+INSERT INTO tb_alunos(nome, matricula, cpf, email, nota) 
+VALUES ("Sharpey Evans", "25847986351", "25436598745", "sharpey.evans@aluno.com", 10);
 
-insert into tb_alunos(nome, matricula, cpf, email, nota) 
-values ("Ryan Evans", "25847857651", "25836365745", "ryan.evans@aluno.com", 10);
+INSERT INTO tb_alunos(nome, matricula, cpf, email, nota) 
+VALUES ("Ryan Evans", "25847857651", "25836365745", "ryan.evans@aluno.com", 10);
 
-insert into tb_alunos(nome, matricula, cpf, email, nota) 
-values ("Chad Danforth", "27897989521", "25485598844", "chad.danforth@aluno.com", 6.4);
+INSERT INTO tb_alunos(nome, matricula, cpf, email, nota) 
+VALUES ("Chad Danforth", "27897989521", "25485598844", "chad.danforth@aluno.com", 6.4);
 
-insert into tb_alunos(nome, matricula, cpf, email, nota) 
-values ("Zeke Baylor", "23897871231", "21683492814", "zeke.baylor@aluno.com", 6.0);
+INSERT INTO tb_alunos(nome, matricula, cpf, email, nota) 
+VALUES ("Zeke Baylor", "23897871231", "21683492814", "zeke.baylor@aluno.com", 6.0);
 
-insert into tb_alunos(nome, matricula, cpf, email, nota) 
-values ("Kelsi Nielsen", "83858471311", "34683487414", "kelsi.nielsen@aluno.com", 9.5);
+INSERT INTO tb_alunos(nome, matricula, cpf, email, nota) 
+VALUES ("Kelsi Nielsen", "83858471311", "34683487414", "kelsi.nielsen@aluno.com", 9.5);
 
-insert into tb_alunos(nome, matricula, cpf, email, nota) 
-values ("Taylor McKessie", "54818471241", "21782457114", "taylor.mckessie@aluno.com", 10);
+INSERT INTO tb_alunos(nome, matricula, cpf, email, nota) 
+VALUES ("Taylor McKessie", "54818471241", "21782457114", "taylor.mckessie@aluno.com", 10);
 
-select * from tb_alunos;
-select * from tb_alunos where nota > 7;
-select * from tb_alunos where nota < 7;
+SELECT * FROM tb_alunos;
+SELECT * FROM tb_alunos WHERE nota > 7;
+SELECT * FROM tb_alunos WHERE nota < 7;
 
-update tb_alunos set nota = "6.5" where id = 1;
+UPDATE tb_alunos SET nota = "6.5" WHERE  id = 1;
 
